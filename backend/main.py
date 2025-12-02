cf78675c53b60f96839025460e07490f05d26f81
Parth Gupta <guptaparth2209@gmail.com>
2025-12-02 19:03:37 +0530

    vercel address added to cors

--- backend/main.py
+++ backend/main.py
@@ -26,7 +26,10 @@ app = FastAPI(
 # CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+        "https://mind-trace-beta.vercel.app",
+        "https://*.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
