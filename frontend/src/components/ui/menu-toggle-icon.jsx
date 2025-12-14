0b51517006daf47a6ac87a4b91b43e45714e3645
Parth Gupta <guptaparth2209@gmail.com>
2025-12-14 14:40:40 +0530

    import fix #2

--- frontend/src/components/ui/menu-toggle-icon.jsx
+++ frontend/src/components/ui/menu-toggle-icon.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { cn } from '../../lib/utils';
-import './burger-menu.css';
+import 'frontend/src/styles/burger-menu.css';
 
 export function MenuToggleIcon({
   open,
